1952e918c67619aaa30e2cf8a4ef8fe8467a5a65
bipan <bipan@bipans-MacBook-Air.local>
2026-01-28 15:56:06 +0545

    homepage react

---
+++ fronted-react/src/assets/components/Footer.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+function Footer() {
+  return (
+    <>
+      <footer className='footer py-3 my-3'>
+        <hr className='border-bottom' />
+        <p className='text-light text-center'>&copy; 2024 - Built by Dhanraj Magar</p>
+
+      </footer>
+      
+    </>
+  )
+}
+
+export default Footer
---
+++ fronted-react/src/assets/components/Main.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Button from "./Button";
+
+const Main = () =>{
+    return (
+      <>
+        <div className="container">
+            <div className="p-5 text-center bg-light-dark rounded">
+                <h1 className="text-light">Stock Prditction Portal</h1>
+                <p className="text-light lead">This stock predition Curious which components explicitly require our JavaScript and Popper? Click the show components link below. If you’re at all unsure about the general page structure, keep reading for an example page template.</p>
+                <Button text="Login" class="btn-outline-info" />
+
+            </div>
+        </div>
+      
+      </>
+    )
+}
+
+export default Main;
